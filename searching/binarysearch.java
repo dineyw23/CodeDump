f785a5f8256d83bd0ac1a6a5b50cab9c678fca9e
dineyw23 <diney.12323@gmail.com>
2016-09-20 14:52:36 -0700

    Added binary search recurssive

--- searching/binarysearch.java
+++ searching/binarysearch.java
@@ -20,18 +20,32 @@ class binarysearch
       }
       return -1;
    }
+   
+   static int binarySearch(int[] array,int x,int low,int high)
+   {
+      int mid = (low + high) / 2;
+      if(high < low)
+         return -1;
+      if(x > array[mid])
+         return binarySearch(array,x,mid + 1,high);
+      else if(x < array[mid])      
+         return binarySearch(array,x,low,mid - 1);
+      else 
+         return mid;
+   }
 
    public static void main(String args[])
    {
       int a[];
       a = new int[5];
       a[0] = 1;
-      a[1] = -12;
+      a[1] = 121;
       a[2] = 0;
       a[3] = 121;
       a[4] = 12;
       
       System.out.println(binarySearch(a,0));
+      System.out.println(binarySearch(a,121,0,4));
    }
 
 }
